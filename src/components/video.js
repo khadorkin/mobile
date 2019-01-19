2d051d61ac78a4d929ffa950910cf5f689b4ee79
alanlanglois <alan@etaminstudio.com>
2019-01-19 18:48:37 +0100

    adding support for fullscreen

--- src/components/video.js
+++ src/components/video.js
@@ -1,7 +1,7 @@
 // @flow
 
 import * as React from 'react';
-import {StyleSheet, Platform} from 'react-native';
+import {Image, Platform, StyleSheet, TouchableOpacity, View} from 'react-native';
 import VideoPlayer from 'react-native-video-controls';
 
 type Props = {|
@@ -11,64 +11,88 @@ type Props = {|
 |};
 
 const styles = StyleSheet.create({
-  backgroundVideo: {
+  container: {
     backgroundColor: '#000',
+    flex: 1
+  },
+  video: {
     position: 'absolute',
     top: 0,
-    left: 0,
+    right: 0,
     bottom: 0,
-    right: 0
+    left: 0
+  },
+  btnExpand: {
+    alignSelf: 'flex-end',
+    width: 15,
+    height: 15,
+    padding: 15,
+    margin: 15
   }
 });
 
 let isVideoReady: boolean = false;
+let isFullScreen: boolean = false;
 
 class Video extends React.PureComponent<Props> {
   props: Props;
 
   componentDidMount = () => {
-    console.log("VIDEO MOUNTED")
     isVideoReady = false;
   };
 
   handleVideoReady = () => {
-    console.log('ON VIDEO READY BUT');
     if (Platform.OS === 'android' && !isVideoReady) {
       isVideoReady = true;
       this.videoPlayer.seekTo(0);
-      console.log('ON VIDEO READY');
     }
   };
 
-  handleEnterFullscreen = () => {
-    console.log('ENTER FULLSCREEN');
+  handleFullscreen = () => {
+    if (!isFullScreen) {
+      isFullScreen = true;
+      this.videoPlayer.player.ref.presentFullscreenPlayer();
+    } else {
+      isFullScreen = false;
+      this.videoPlayer.player.ref.dismissFullscreenPlayer();
+    }
   };
-
-  handleExitFullscreen = () => {
-    console.log('EXIT FULLSCREEN');
-  }
 
   render() {
     const {source, preview, testID} = this.props;
 
     return (
-      <VideoPlayer
-        testID={testID}
-        source={{
-          uri: source
-        }} // Can be a URL or a local file.
-        ref={ref => {
-          this.videoPlayer = ref;
-        }}
-        style={styles.backgroundVideo}
-        poster={preview}
-        resizeMode="contain"
-        onEnterFullscreen={this.handleEnterFullscreen}
-        onExitFullscreen={this.handleExitFullscreen}
-        onReadyForDisplay={this.handleVideoReady}
-        disableVolume
-        disableBack
-      />
+      <View style={styles.container}>
+        <VideoPlayer
+          testID={testID}
+          source={{
+            uri: source
+          }} // Can be a URL or a local file.
+          ref={ref => {
+            this.videoPlayer = ref;
+          }}
+          style={styles.video}
+          poster={preview}
+          resizeMode="contain"
+          onReadyForDisplay={this.handleVideoReady}
+          disableVolume
+          disableBack
+          disableFullscreen
+        />
+        <TouchableOpacity
+          activeOpacity={0.3}
+          onPress={() => {
+            this.handleFullscreen();
+          }}
+        >
+          <Image
+            width={15}
+            height={15}
+            style={styles.btnExpand}
+            source={{uri: 'https://reddingdesigns.com/images/icons/icon-expand-white.png'}}
+          />
+        </TouchableOpacity>
+      </View>
     );
   }
 }
--- src/components/video.stories.js
+++ src/components/video.stories.js
@@ -7,7 +7,7 @@ import Video from './video';
 
 storiesOf('Video', module).add('Default', () => (
   <Video
-    testID="basic-text"
+    testID="basic-video"
     source="https://alanlanglois.net/yob/yob_hurricane.mp4"
     preview="https://alanlanglois.net/yob/preview.png"
   />
