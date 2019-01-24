8e1316d079da2db6bf169a59f3b63243a709bb17
Vincent Catillon <devotion95@gmail.com>
2019-01-24 10:36:35 +0100

    Add video component

--- src/components/video.js
+++ src/components/video.js
@@ -1,17 +1,34 @@
 // @flow
 
 import * as React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, TouchableOpacity} from 'react-native';
 import VideoPlayer from 'react-native-video-controls';
+import {NovaSolidDesignActionsRedo} from '@coorpacademy/nova-icons';
 
 import theme from '../modules/theme';
+import Preview from './preview';
+import ResourceOverlay from './resource-overlay';
+
+export type Step = 'preview' | 'play' | 'end';
 
 type Props = {|
   source: File | {uri: string},
+  preview: File | {uri: string},
+  step: Step,
+  isFullScreen?: boolean,
+  onPlay: () => void,
+  onEnd: () => void,
+  onReady: () => void,
   onExpand?: () => void,
   onShrink?: () => void,
   onRef?: (VideoPlayer | null) => void
 |};
+
+export const STEP: {[key: string]: Step} = {
+  PREVIEW: 'preview',
+  PLAY: 'play',
+  END: 'end'
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -32,22 +49,46 @@ const styles = StyleSheet.create({
   }
 });
 
-const Video = ({source, onExpand, onShrink, onRef}: Props) => (
+const Video = ({
+  source,
+  preview,
+  step,
+  isFullScreen,
+  onPlay,
+  onReady,
+  onEnd,
+  onExpand,
+  onShrink,
+  onRef
+}: Props) => (
   <View style={styles.container}>
-    <VideoPlayer
-      testID="video"
-      source={source}
-      ref={onRef}
-      style={styles.video}
-      resizeMode="contain"
-      disableVolume
-      disableBack
-      disableFullscreen={Boolean(!onExpand && !onShrink)}
-      toggleResizeModeOnFullscreen={false}
-      onEnterFullscreen={onExpand}
-      onExitFullscreen={onShrink}
-      onFullscreenPlayerWillDismiss={onShrink}
-    />
+    {step === STEP.PREVIEW && <Preview type="video" source={preview} onButtonPress={onPlay} />}
+    {step === STEP.PLAY && (
+      <VideoPlayer
+        testID="video"
+        source={source}
+        ref={onRef}
+        style={styles.video}
+        resizeMode="contain"
+        disableVolume
+        disableBack
+        disableFullscreen={Boolean(!onExpand && !onShrink)}
+        toggleResizeModeOnFullscreen={false}
+        isFullScreen={isFullScreen}
+        onEnterFullscreen={onExpand}
+        onExitFullscreen={onShrink}
+        onFullscreenPlayerWillDismiss={onShrink}
+        onEnd={onEnd}
+        onReadyForDisplay={onReady}
+      />
+    )}
+    {step === STEP.END && (
+      <ResourceOverlay>
+        <TouchableOpacity onPress={onPlay}>
+          <NovaSolidDesignActionsRedo color={theme.colors.white} height={40} width={40} />
+        </TouchableOpacity>
+      </ResourceOverlay>
+    )}
   </View>
 );
 
