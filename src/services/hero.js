ee83b8b81ca710859d1263de6c84d6bba10663a0
chrisdugne <chris.dugne@uralys.com>
2019-09-25 15:26:09 +0200

    fixed merge

--- src/services/hero.js
+++ src/services/hero.js
@@ -8,18 +8,10 @@ export type HeroService = {|
   get: () => Promise<DisciplineCard | ChapterCard | void>
 |};
 
-<<<<<<< HEAD
-const get = async (dataLayer: DataLayer) => {
-  const {fetchRecommendation, getHeroContent, getAllProgressions} = dataLayer;
-  const progressions = await getAllProgressions();
-  const aggregations = aggregate(progressions);
-  return getHeroContent(aggregations, fetchRecommendation);
-=======
 const get = (dataLayer: DataLayer) => async () => {
   const {fetchCard, fetchRecommendation, getHeroContent} = dataLayer;
   const aggregations = await getAggregationsByContent();
   return getHeroContent(aggregations, fetchRecommendation, fetchCard);
->>>>>>> moced aggregations within layer/data/progressions
 };
 
 const service = (dataLayer: DataLayer): HeroService => ({
